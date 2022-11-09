8fcb91030d5f05231711ccd5b0463aa2b1cd3ee1
khatrisaugat <khatri.saugat55@gmail.com>
2022-11-09 16:21:55 +0545

    initial skeleton and packages

---
+++ src/components/table.component/table.component.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+function Table({}: Props) {
+  return <div>Table</div>;
+}
+
+export default Table;
---
+++ src/static/constants.ts
@@ -0,0 +1,1 @@
+export const API_URL: String = "https://graphqlzero.almansi.me/api";
